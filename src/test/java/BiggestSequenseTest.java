d4000e0ac52cebb290d4366562f673ec9fa16242
Alekseev Yaroslav <alekseevy509@gmail.com>
2019-09-04 21:31:08 +0300

    Max sequence divide on previous element

--- src/test/java/BiggestSequenseTest.java
+++ src/test/java/BiggestSequenseTest.java
@@ -1,4 +1,5 @@
 import com.company.dynamic.BiggestSequence;
+import com.company.dynamic.BiggestSequenceWithDevide;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,6 +10,16 @@ public class BiggestSequenseTest {
     public void testFindBiggestSequence() throws Exception {
         int actual = BiggestSequence.run(new Scanner("4\n3\n6\n7\n12"));
         int expected = 4;
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void testFindBiggestSequenceDevide() throws Exception {
+        int actual = BiggestSequenceWithDevide.run(new Scanner("4\n3\n6\n7\n12"));
+        int expected = 3;
 
         Assert.assertEquals(expected, actual);
 
