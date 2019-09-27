2c302cdbfeb470464eb6a9f65209563ea9ccbe7d
Alekseev Yaroslav <alekseevy509@gmail.com>
2019-09-27 20:34:32 +0300

    Money task https://stepik.org/lesson/13262/step/3?unit=3447

---
+++ src/main/java/com/company/dynamic/Money.java
@@ -0,0 +1,53 @@
+package com.company.dynamic;
+
+public class Money {
+    private int[] D;
+    private int[] C;
+    private int n;
+
+    public static void main(String[] args) {
+        Money money = new Money();
+        money.setN(10);
+        money.setD(new int[10 + 1]);
+        money.setC(new int[]{1, 5, 10});
+        System.out.println(money.changer());
+    }
+
+    public int changer() {
+        D[0] = 0;
+        D[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            D[i] = Integer.MAX_VALUE;
+            for (int j = 0; j < C.length; j++) {
+                if (C[j] <= i) {
+                    D[i] = Math.min(D[i], D[i - C[j]] + 1);
+                }
+            }
+        }
+        return D[n];
+    }
+
+    public int[] getD() {
+        return D;
+    }
+
+    public void setD(int[] d) {
+        D = d;
+    }
+
+    public int[] getC() {
+        return C;
+    }
+
+    public void setC(int[] c) {
+        C = c;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+}
